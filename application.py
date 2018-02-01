c9afc7ad73a9c2d25f0fb42eea4a98df01f3f842
Manan Hora <horamanan@gmail.com>
2018-02-01 16:14:47 -0500

    added venv

--- application.py
+++ application.py
@@ -4,33 +4,33 @@ application = Flask(__name__) #instance of application
 
 
 
-from application import db
-from application.models import Data
-from flask.ext.sqlalchemy import SQLAlchemy
-from application.forms import EnterDBInfo, RetrieveDBInfo
-import os
-from models import Result
+# from application import db
+# from application.models import Data
+# from flask.ext.sqlalchemy import SQLAlchemy
+# from application.forms import EnterDBInfo, RetrieveDBInfo
+# import os
+# from models import Result
 
 
-application.debug=True
-
-
-
-application.config.from_object(os.environ['APP_SETTINGS'])
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(application)
+# application.debug=True
 
 
 
-@app.route('/')
+# application.config.from_object(os.environ['APP_SETTINGS'])
+# application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(application)
+
+
+
+@application.route('/')
 def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+# @app.route('/<name>')
+# def hello_name(name):
+#     return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
