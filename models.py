88ad877fb7969b0578e124849d72876bc010560a
Manan Hora <horamanan@gmail.com>
2018-02-05 22:13:27 -0500

    added alembic, models.py, manage.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from application import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String()) # NEED TO ADD ENCRYPTION
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
