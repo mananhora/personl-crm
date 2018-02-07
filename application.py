5e88e3471cc3dad9d65961e405fc464850900c99
Manan Hora <horamanan@gmail.com>
2018-02-07 11:46:21 -0500

    added '+psycopg2' to db url

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ application = Flask(__name__) #instance of application
 #  "postgresql://mananhora@localhost/messamis"
 
 
-application.config['SQLALCHEMY_DATABASE_URI'] = "postgres://kabpjhzvtsincy:da9b7e11e66d93cb77f054465b4c399742e87555c4129b038a56eda6626014e9@ec2-184-73-201-79.compute-1.amazonaws.com:5432/d1d5c4sdakh9ip"
+application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://kabpjhzvtsincy:da9b7e11e66d93cb77f054465b4c399742e87555c4129b038a56eda6626014e9@ec2-184-73-201-79.compute-1.amazonaws.com:5432/d1d5c4sdakh9ip"
 # application.config['DEBUG'] = True
 
 db = SQLAlchemy(application)
