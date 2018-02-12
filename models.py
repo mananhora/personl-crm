e7d1b253407343a7ccb28920c5b530c7b2085dcc
Manan Hora <horamanan@gmail.com>
2018-02-12 11:50:57 -0500

    added circle and friend classes

--- models.py
+++ models.py
@@ -20,3 +20,19 @@ class User(db.Model):
     #how we want the object to be represented when it's printed
     def __repr__(self):
         return '<email {}>'.format(self.email)
+
+
+class Circle(db.Model):
+    #describe circle table here
+    __tablename__ = 'circles'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+
+
+class Friend(db.Model):
+    #describe friend table here
+    __tablename__ = 'friends'
+    id = db.Column(db.Integer, primary_key=True)
+
+
