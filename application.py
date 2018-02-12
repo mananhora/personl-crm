cb595df33423c446564bb44ef9b872091d6606a8
Manan Hora <horamanan@gmail.com>
2018-02-12 11:50:08 -0500

    minor changes, added imports

--- application.py
+++ application.py
@@ -1,17 +1,19 @@
 import os
 import requests
-from flask import Flask, render_template, request, session, flash, g
+from flask import Flask, render_template, request, session, flash, g, redirect, url_for
 from functools import wraps
-port = int(os.environ.get("PORT", 5433))
 from flask_sqlalchemy import SQLAlchemy
+# import models, config
 
+
+port = int(os.environ.get("PORT", 5433))
 
 application = Flask(__name__) #instance of application
 
 
 #  "postgresql://mananhora@localhost/messamis"
 
-application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mananhora@localhost/messamis"
+application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mananhora@localhost/mesamis"
 application.secret_key = "wahe guru"
 
 
@@ -21,9 +23,10 @@ application.secret_key = "wahe guru"
 # application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://kabpjhzvtsincy:da9b7e11e66d93cb77f054465b4c399742e87555c4129b038a56eda6626014e9@ec2-184-73-201-79.compute-1.amazonaws.com:5432/d1d5c4sdakh9ip"
 # application.config['DEBUG'] = True
 
-db = SQLAlchemy(application) #create instance of sql alchemy with application as parameter 
 
-import models, config
+db = SQLAlchemy(application) #create instance of sql alchemy with application as parameter
+
+
 
 # login required decorator
 def login_required(f):
@@ -37,13 +40,12 @@ def login_required(f):
     return wrap
 
 
-
 @application.route('/')
 @login_required
 def home():
-	g.db = connect_db() #connect to database
-	#perform queries here..
-	g.db.close() #close connection with database..
+	# g.db = connect_db() #connect to database
+	# #perform queries here..
+	# g.db.close() #close connection with database..
     return render_template('index.html')  # render a template
 
 
@@ -51,7 +53,8 @@ def home():
 def hello_name(name):
     return "Hello {}!".format(name)
 
-@app.route('/welcome')
+
+@application.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
 
@@ -72,7 +75,6 @@ def login():
 	return render_template('index.html', error=error)
 
 
-
 @application.route('/logout', methods=['GET', 'POST'])
 def logout():
 	error = None
@@ -80,19 +82,6 @@ def logout():
 	print ('logged out')
 	flash('you were just logged out')
 	return redirect(url_for('welcome'))
-
-# connect to database
-
-def connect_db():
-    #add code here..
-
-
-
-
-
-
-
-
 
 
 
