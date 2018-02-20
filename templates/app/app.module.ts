0317a9414549268208a1201b1e87b8fb0586b068
tarinrickett <trickett96@gmail.com>
2018-02-19 23:43:44 -0500

    add nodes module

--- templates/app/app.module.ts
+++ templates/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { NodesComponent } from './nodes/nodes.component';
 
 
 @NgModule({
