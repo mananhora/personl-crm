350d5c771cd56b07e8a32f952da847f7f4196590
Manan Hora <horamanan@gmail.com>
2018-02-21 16:40:50 -0500

    added the friend creation form

--- form.py
+++ form.py
@@ -13,3 +13,9 @@ class RegistrationForm(Form):
     email = TextField('email', validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]) #datarequired validator ensures that the form is not empty
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=25)])
     confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+
+
+class FriendCreationForm(Form):
+    name = TextField('name', validators=[DataRequired()])
+    email = TextField('email', validators=[Email(message=None), Length(min=6, max=40)])  # datarequired validator ensures that the form is not empty
+    location = TextField('location')
