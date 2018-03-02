b800e8de5d82b379cce0abf56d00037ce90e5a42
Manan Hora <horamanan@gmail.com>
2018-03-01 20:44:55 -0500

    updated from front end branch

--- project/home/functions.py
+++ project/home/functions.py
@@ -24,21 +24,16 @@ def get_friends():
     if current_user is not None:
       if current_user.id is not None:
         user_id = current_user.id
-        user = User.query.get(user_id)
-        print (user.id)
-        friends = user.friends.all()
-        print(friends)
-        # friends = Friend.query.filter_by(user_id=user_id).all()
-        # for i in range(0, len(friends)):
-        #   print(friends[i].name)
+        friends = Friend.query.filter_by(user_id=user_id).all()
+        for i in range(0, len(friends)):
+          print(friends[i].name)
     # flash(str(friends.name))
     return render_template('index.html')
 
 
 @home_blueprint.route('/')
-@login_required
 def home():
     # g.db = connect_db() #connect to database
     # #perform queries here..
     # g.db.close() #close connection with database..
-    return render_template('index.html')  # render a template
+    return app.send_static_file('dist/index.html')  # render a template
