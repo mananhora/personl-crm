160aafb05db763f738b1c9bcf9191cb2c1443ab8
tarinrickett <trickett96@gmail.com>
2018-03-05 14:55:51 -0500

    progress on profile component

--- project/static/app/profile/profile.component.ts
+++ project/static/app/profile/profile.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  model = {
+    name: 'first last',
+    location: 'university of rochester',
+    img: 'https://pbs.twimg.com/profile_images/684222363907551232/54R_VAhv_400x400.jpg',
+    circles: ['circle a', 'circle b', 'third circle name'],
+    email: 'name@name.com',
+    phone: '+15555551234',
+  };
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
