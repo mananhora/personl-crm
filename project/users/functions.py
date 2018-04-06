66b018fcace36c43cb6e26bb9f11a345ea28e0e6
Manan Hora <horamanan@gmail.com>
2018-04-06 13:43:37 -0400

    updates

--- project/users/functions.py
+++ project/users/functions.py
@@ -34,10 +34,6 @@ from facebook import get_user_from_cookie, GraphAPI
 from flask import g, render_template, redirect, request, session, url_for
 
 
-
-
-
-
 @users_blueprint.route('/register', methods=['POST'])
 def register():
     print("in register")
@@ -93,84 +89,96 @@ def login():
 
 
 
-@users_blueprint.route('/loginwithfb', methods=['POST'])
-def login_with_fb():
-  return None
-
-
-@app.route('/fblogin')
+@users_blueprint.route('/loginwithfb', methods=['POST', 'GET'])
 def index():
     # If a user was set in the get_current_user function before the request,
     # the user is logged in.
+    g.user = None
     if g.user:
       print("g. user")
       print(g.user)
       return render_template('index.html', app_id=FB_APP_ID,
                                app_name=FB_APP_NAME, user=g.user)
     # Otherwise, a user is not logged in.
+    print("i guess there was no g.user..")
+    print("redirecting to login.html..")
     return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
 
 
-@app.before_request
-def get_current_user():
-    """Set g.user to the currently logged in user.
-
-    Called before each request, get_current_user sets the global g.user
-    variable to the currently logged in user.  A currently logged in user is
-    determined by seeing if it exists in Flask's session dictionary.
-
-    If it is the first time the user is logging into this application it will
-    create the user and insert it into the database.  If the user is not logged
-    in, None will be set to g.user.
-    """
-
-    # Set the user in the session dictionary as a global g.user and bail out
-    # of this function early.
-    if session.get('user'):
-        g.user = session.get('user')
-        return
-
-    # Attempt to get the short term access token for the current user.
-    result = get_user_from_cookie(cookies=request.cookies, app_id=FB_APP_ID,
-                                  app_secret=FB_APP_SECRET)
-
-    # If there is no result, we assume the user is not logged in.
-    if result:
-        # Check to see if this user is already in our database.
-        # user = User.query.filter(User.id == result['uid']).first()
-
-        user = None
-
-        if not user or user is None:
-            # Not an existing user so get info
-            graph = GraphAPI(result['access_token'])
-            profile = graph.get_object('me')
-            if 'link' not in profile:
-                profile['link'] = ""
-
-            # Create the user and insert it into the database
-            user = User(name=profile['name'],
-                        profile_url=profile['link'],
-                        access_token=result['access_token'])
-            db.session.add(user)
-        elif user.access_token != result['access_token']:
-            # If an existing user, update the access token
-            user.access_token = result['access_token']
-
-        # Add the user to the current session
-        session['user'] = dict(name=user.name, profile_url=user.profile_url,
-                               id=user.id, access_token=user.access_token)
-
-    # Commit changes to the database and set the user as a global g.user
-    db.session.commit()
-    g.user = session.get('user', None)
+# @app.before_request
+# def get_current_user():
+#     print ("in get current user")
+#     """Set g.user to the currently logged in user.
+#
+#     Called before each request, get_current_user sets the global g.user
+#     variable to the currently logged in user.  A currently logged in user is
+#     determined by seeing if it exists in Flask's session dictionary.
+#
+#     If it is the first time the user is logging into this application it will
+#     create the user and insert it into the database.  If the user is not logged
+#     in, None will be set to g.user.
+#     """
+#
+#     # Set the user in the session dictionary as a global g.user and bail out
+#     # of this function early.
+#     if session.get('user'):
+#         g.user = session.get('user')
+#         print ("got user")
+#         print(g.user)
+#         return
+#
+#
+#     print("getting user from cookie")
+#     # Attempt to get the short term access token for the current user.
+#     result = get_user_from_cookie(cookies=request.cookies, app_id=FB_APP_ID,
+#                                   app_secret=FB_APP_SECRET)
+#     print("result...")
+#     print(result)
+#
+#     # If there is no result, we assume the user is not logged in.
+#     if result:
+#
+#         # Check to see if this user is already in our database.
+#         # user = User.query.filter(User.id == result['uid']).first()
+#
+#         user = None
+#
+#         if not user or user is None:
+#             print ("not an existing user")
+#             # Not an existing user so get info
+#             graph = GraphAPI(result['access_token'])
+#             profile = graph.get_object('me')
+#             if 'link' not in profile:
+#                 profile['link'] = ""
+#
+#             # Create the user and insert it into the database
+#             user = User(name=profile['name'],
+#                         profile_url=profile['link'],
+#                         access_token=result['access_token'])
+#             db.session.add(user)
+#         elif user.access_token != result['access_token']:
+#             # If an existing user, update the access token
+#             user.access_token = result['access_token']
+#
+#         # Add the user to the current session
+#         session['user'] = dict(name=user.name, profile_url=user.profile_url,
+#                                id=user.id, access_token=user.access_token)
+#
+#     # Commit changes to the database and set the user as a global g.user
+#     print("i guess there was no result...")
+#     db.session.commit()
+#     g.user = session.get('user', None)
 
 
 @login_required
 @users_blueprint.route('/userinfo', methods = ['GET'] )
 def get_user_info():
   #send info about user
-  return jsonify(current_user.serialize)
+  if current_user and current_user is not None and current_user.is_anonymous==False:
+    return jsonify(current_user.serialize)
+  else:
+    flash("not logged in!")
+    print("not logged in!")
 
 
 
