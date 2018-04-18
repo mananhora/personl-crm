1fa25e872431cdd5e1b9917f926587b5505b715d
Manan Hora <horamanan@gmail.com>
2018-04-18 12:55:33 -0400

    get_reminders is working
    - set last contacted, set frequency: to be tested with front end in
    place

--- project/reminders/functions.py
+++ project/reminders/functions.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import Blueprint, jsonify
 from flask.ext.login import current_user, login_required
@@ -57,18 +57,19 @@ def get_reminders():
     if current_user.id is not None and a == False:
       for friend in current_user.friends:
         now = datetime.utcnow()
-        reminder_date = get_next_contact_date(friend.last_contacted, friend.num_weeks_reminder)
-        num_days_left = abs((reminder_date - now).days)
-        if(num_days_left<8):
-          reminders_list.append({friend.serialize, num_days_left})
+        if(friend.last_contacted is not None and friend.num_weeks_reminder is not None):
+          reminder_date = get_next_contact_date(friend.last_contacted, friend.num_weeks_reminder)
+          num_days_left = abs((reminder_date - now).days)
+          if(num_days_left<8):
+            reminders_list.append([friend.serialize, num_days_left])
       return (jsonify(reminders_list))
   return (jsonify({"result":"Error"}))
 
 
 def get_next_contact_date(last_contacted_date, n):
   num_days_to_add = n*7
-  end_date = last_contacted_date + datetime.timedelta(days=num_days_to_add)
-
+  end_date = last_contacted_date + timedelta(days=num_days_to_add)
+  return end_date
 
 
 
