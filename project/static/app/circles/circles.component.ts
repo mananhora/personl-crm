7c07d5ecad48b7dd9fd5f656b5ad573a81b0d221
tarinrickett <trickett96@gmail.com>
2018-04-23 14:57:03 -0400

    circles render as nodes!

--- project/static/app/circles/circles.component.ts
+++ project/static/app/circles/circles.component.ts
@@ -3,59 +3,79 @@ import { CirclesService } from './circles.service';
 import { FriendsService } from '../friends/friends.service';
 import { Circle } from './circle';
 import { Profile } from '../profile/profile';
+import {
+  VisNode,
+  VisNodes,
+  VisEdges,
+  VisNetworkService,
+  VisNetworkData,
+  VisNetworkOptions
+} from '../../../../node_modules/ng2-vis/components/network';
+
+class ExampleNetworkData implements VisNetworkData {
+    public nodes: VisNodes;
+    public edges: VisEdges;
+}
 
 @Component({
   selector: 'app-circles',
   templateUrl: './circles.component.html',
   styleUrls: ['./circles.component.css']
 })
+
 export class CirclesComponent implements OnInit {
 
   circles: Circle[];
   friends: Profile[];
 
-  constructor(private circlesService: CirclesService,
-    private friendsService: FriendsService) { }
+  public nodes = new VisNodes([ { id: '0', label: 'myVillage' } ]);
+  public edges = new VisEdges([ ]);
+  public visNetwork: string = 'networkId1';
+  public visNetworkData: ExampleNetworkData;
+  public visNetworkOptions: VisNetworkOptions;
 
-  showAllCircles() {
+  constructor(private circlesService: CirclesService,
+    private friendsService: FriendsService,
+    private visNetworkService: VisNetworkService) { }
+
+  getAllCircles() {
     this.circlesService.getCircles()
       .subscribe(data => {
         for (let i = 0; i < data['json_list'].length; i++) {
           let name = data['json_list'][i]['circle_name'];
           let id = data['json_list'][i]['id'];
-          let circle = new Circle(name, id);
 
-          if (this.circles) {
-            this.circles.push(circle);
-          } else {
-            this.circles = [circle];
-          }
+          this.visNetworkData.nodes.add({ id: id, label: name });
+          this.visNetworkService.fit(this.visNetwork);
+          this.edges.add({ from: '0', to: id });
         }
     });
   }
 
-  // getFriends(id: number) {
-  //   console.log('yo: tryna get friends');
-  //   this.friendsService.getFriends(id)
-  //     .subscribe(data => {
-  //       console.log('yo: found ', data['json_list']);
-  //       for (let i = 0; i < data['json_list'].length; i++) {
-  //         if (this.friends) {
-  //           this.friends.push(data['json_list'][i]['name']);
-  //         } else {
-  //           this.friends = [ data['json_list'][i]['name'] ];
-  //         }
-  //       }
-  //       if (this.friends) {
-  //         this.friends.push('example name');
-  //       } else {
-  //         this.friends = [ 'example name' ];
-  //       }
-  //   });
-  // }
+   public networkInitialized(): void {
+     // now we can use the service to register on events
+     this.visNetworkService.on(this.visNetwork, 'click');
+
+     // open your console/dev tools to see the click params
+     this.visNetworkService.click
+       .subscribe((eventData: any[]) => {
+           if (eventData[0] === this.visNetwork) {
+             console.log(eventData[1]);
+           }
+       });
+   }
 
   ngOnInit() {
-    this.showAllCircles();
+    this.getAllCircles();
+
+    const nodes = this.nodes;
+    const edges = this.edges;
+    this.visNetworkData = {
+      nodes,
+      edges,
+    };
+
+    this.visNetworkOptions = {};
   }
 
 }
